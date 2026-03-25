3db36aea45e4bb5b67ff69c4d200338c49a69177
sriharigacharya <sriharigacharya@gmail.com>
2026-03-25 22:54:39 +0530

    Add RedisTemplate bean to RedisConfig

--- realtime-chat-backend/src/main/java/com/chat/realtime/config/RedisConfig.java
+++ realtime-chat-backend/src/main/java/com/chat/realtime/config/RedisConfig.java
@@ -4,14 +4,26 @@ import com.chat.realtime.service.RedisMessageSubscriber;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
     public static final String CHAT_TOPIC = "chatRoomTopic";
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
 
     @Bean
     public ChannelTopic topic() {
