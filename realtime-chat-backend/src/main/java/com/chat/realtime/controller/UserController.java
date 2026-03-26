47c0ebde4b5d8a5b076ae95381f72d106e03d15e
sriharigacharya <sriharigacharya@gmail.com>
2026-03-26 15:15:59 +0530

    Fixes

--- realtime-chat-backend/src/main/java/com/chat/realtime/controller/UserController.java
+++ realtime-chat-backend/src/main/java/com/chat/realtime/controller/UserController.java
@@ -1,17 +1,22 @@
 package com.chat.realtime.controller;
 
 import com.chat.realtime.model.User;
+import com.chat.realtime.repository.UserRepository;
 import com.chat.realtime.service.UserService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final UserRepository userRepository;
 
     @PostMapping("/register")
     public ResponseEntity<UserDto> register(@RequestBody AuthRequest request) {
@@ -23,6 +28,15 @@ public class UserController {
     public ResponseEntity<UserDto> login(@RequestBody AuthRequest request) {
         User user = userService.login(request.getUsername(), request.getPassword());
         return ResponseEntity.ok(new UserDto(user.getId(), user.getUsername()));
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserDto>> getUsers() {
+        List<UserDto> users = userRepository.findAll()
+                .stream()
+                .map(u -> new UserDto(u.getId(), u.getUsername()))
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(users);
     }
 
     @Data
